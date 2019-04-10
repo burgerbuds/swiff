2adf86572aa1006ad7fd27069158089608957873
Ben Rogerson <info@benrogerson.com.au>
2019-04-11 00:00:07 +0930

    slit out sql queries for reuse

--- src/database.js
+++ src/database.js
@@ -3,6 +3,24 @@ import mysql from 'promise-mysql'
 import { cmdPromise } from './utils'
 import { isEmpty } from './utils'
 
+// A better solution than dropping and recreating the db.
+// It also avoids errors when run on an empty database.
+// https://stackoverflow.com/a/18625545
+const dropDbQuery = `SET FOREIGN_KEY_CHECKS = 0;
+SET GROUP_CONCAT_MAX_LEN=32768;
+SET @tables = NULL;
+SELECT GROUP_CONCAT('\`', table_name, '\`') INTO @tables
+  FROM information_schema.tables
+  WHERE table_schema = (SELECT DATABASE());
+SELECT IFNULL(@tables,'dummy') INTO @tables;
+SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
+PREPARE stmt FROM @tables;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+SET FOREIGN_KEY_CHECKS = 1;`
+
+const importDbQuery = ({ host = 'localhost', port = 3306, user, password, database, importFile }) => `mysql --host='${host}' --port='${!isEmpty(port) ? port : 3306}' --user='${user}' --password='${password}' ${database} < ${importFile};`
+
 // Clear out the tables in a database
 const doDropAllDbTables = async config => {
     let errorMessage
@@ -18,29 +36,13 @@ const doDropAllDbTables = async config => {
         .createConnection({ ...defaultConfig, ...config })
         .catch(error => (errorMessage = error))
     if (errorMessage) return new Error(errorMessage)
-    // This query avoids drop errors when run multiple times.
-    // A more solid solution than dropping and recreating the db.
-    // https://stackoverflow.com/a/18625545
-    conn.query(`SET FOREIGN_KEY_CHECKS = 0;
-    SET GROUP_CONCAT_MAX_LEN=32768;
-    SET @tables = NULL;
-    SELECT GROUP_CONCAT('\`', table_name, '\`') INTO @tables
-      FROM information_schema.tables
-      WHERE table_schema = (SELECT DATABASE());
-    SELECT IFNULL(@tables,'dummy') INTO @tables;
-    SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
-    PREPARE stmt FROM @tables;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-    SET FOREIGN_KEY_CHECKS = 1;`)
+    conn.query(dropDbQuery)
     conn.end()
 }
 
-const doImportDb = async ({ host = 'localhost', port = 3306, user, password, database, importFile }) => {
+const doImportDb = async (config) => {
     let errorMessage
-    await cmdPromise(
-        `mysql --host='${host}' --port='${!isEmpty(port) ? port : 3306}' --user='${user}' --password='${password}' ${database} < ${importFile};`
-    ).catch(e => (errorMessage = e))
+    await cmdPromise(importDbQuery(config)).catch(e => (errorMessage = e))
     if (errorMessage) return new Error(errorMessage)
 }
 
@@ -53,7 +55,7 @@ const doLocalDbDump = async config => {
 }
 
 const getDbDumpZipCommands = ({ host = 'localhost', port = 3306, user, password, database, gzipFilePath }) =>
-    // Dump and zip the remote db (can make it around 9 times smaller)
+    // Dump and zip the db - this can make it around 9 times smaller
     `mysqldump --host='${host}' --port='${port}' --user='${user}' --password='${password}' ${database} | gzip > '${gzipFilePath}'`
 
-export { getDbDumpZipCommands, doDropAllDbTables, doImportDb, doLocalDbDump }
+export { getDbDumpZipCommands, doDropAllDbTables, dropDbQuery, doImportDb, doLocalDbDump, importDbQuery }
