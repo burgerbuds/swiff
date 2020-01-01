02ad018ef1a88167601f68ff546558bd529bae72
Ben Rogerson <info@benrogerson.com.au>
2020-01-01 14:27:04 +1030

    linting

--- src/utils.js
+++ src/utils.js
@@ -109,43 +109,45 @@ const paginate = ({
     maxPages = 999,
 }) => {
     // calculate total pages
-    let totalPages = Math.ceil(totalItems / pageSize);
-    let startPage = 1;
-    let endPage = null;
+    let totalPages = Math.ceil(totalItems / pageSize)
+    let startPage = 1
+    let endPage = null
 
     // ensure current page isn't out of range
     if (currentPage < 1) {
-        currentPage = 1;
+        currentPage = 1
     } else if (currentPage > totalPages) {
-        currentPage = totalPages;
+        currentPage = totalPages
     }
     if (totalPages <= maxPages) {
         // total pages less than max so show all pages
-        endPage = totalPages;
+        endPage = totalPages
     } else {
         // total pages more than max so calculate start and end pages
-        let maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
-        let maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
+        let maxPagesBeforeCurrentPage = Math.floor(maxPages / 2)
+        let maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1
         if (currentPage <= maxPagesBeforeCurrentPage) {
             // current page near the start
-            endPage = maxPages;
+            endPage = maxPages
         } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
             // current page near the end
-            startPage = totalPages - maxPages + 1;
-            endPage = totalPages;
+            startPage = totalPages - maxPages + 1
+            endPage = totalPages
         } else {
             // current page somewhere in the middle
-            startPage = currentPage - maxPagesBeforeCurrentPage;
-            endPage = currentPage + maxPagesAfterCurrentPage;
+            startPage = currentPage - maxPagesBeforeCurrentPage
+            endPage = currentPage + maxPagesAfterCurrentPage
         }
     }
 
     // calculate start and end item indexes
-    let startIndex = (currentPage - 1) * pageSize;
-    let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    let startIndex = (currentPage - 1) * pageSize
+    let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1)
 
     // create an array of pages to ng-repeat in the pager control
-    let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+    let pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+        i => startPage + i
+    )
 
     // return object with all pager properties required by the view
     return {
@@ -157,8 +159,8 @@ const paginate = ({
         endPage: endPage,
         startIndex: startIndex,
         endIndex: endIndex,
-        pages: pages
-    };
+        pages: pages,
+    }
 }
 
 export {
